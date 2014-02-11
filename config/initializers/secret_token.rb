56dcfd207a8445ae27f1887c2db67df60830f558
Yasunori Suzuki <railsyas@gmail.com>
2014-02-11 15:10:03 +0900

    remove secret_token.rb from .gitignore.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeckoboardHandson::Application.config.secret_key_base = 'f8f6da4187c6a85f67b3666bfa3930a9bc0eddf8d9569b50b6635c3efe57be55f10ac8045290e976440703177849e7750e48609c16137a83853598b90d5083fa'
